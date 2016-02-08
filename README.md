5d5cd774effaca876bf22eecf3453f9c88905c14
Darren Tyson (office) <darren.tyson@vanderbilt.edu>
2016-02-08 17:34:25 -0600

    Updating README and allowing example dipDRC code to save downloaded data into a temporary file rather than to a user's disk.

--- README.md
+++ README.md
@@ -22,5 +22,18 @@ type:
 source("./example dipDRC/makeDRCexample.R",chdir=TRUE)
 ```
 
+The data used in this example is in the file named: `dipDRC_example_data.csv`
+It is the raw breast cancer cell line (cell counts) data used to make Figure 4: MDA-MB-231 cells treated with 
+rotenone or phenformin. The data are from a single experiment with two technical replicates and are 
+structured as a seven-column matrix with the following column headers:
+1) `time`
+2) `cell.count`
+3) `cell.line`
+4) `drug`
+5) `conc`
+6) `well`
+7) `expt.date`
 
-
+The output of the `dipDRC` function is a graphics window with two plots, one for each cell line and drug condition
+and a list of the two `drm` (dose-response model) objects, described in detail in the package information of the
+`drc` library: [https://cran.r-project.org/web/packages/drc/drc.pdf](https://cran.r-project.org/web/packages/drc/drc.pdf)
--- example dipDRC/makeDRCexample.r
+++ example dipDRC/makeDRCexample.r
@@ -1,5 +1,5 @@
 # if devtools library not insalled, uncomment next line
-# install.packages('devtools')
+#install.packages('devtools')
 
 # libraries necessary to load source files directly from GitHub
 library(devtools)
@@ -9,17 +9,21 @@ library(RCurl)
 SourceURL <- "https://raw.github.com/QuLab-VU/DIP_rate_NatMeth2016/master/dipDRC/dipDRC.r"
 source_url(SourceURL)
 
-# download example data to local directory 
-# comment out 2 lines below to load your own data
+# Download example data (comment out if loading your own data)
+# make temporary file for data to be downloaded
+temp <- tempfile()
+# download example data to temp file 
 download.file("https://raw.githubusercontent.com/QuLab-VU/DIP_rate_NatMeth2016/master/dipDRC/dipDRC_example_data.csv", 
-    destfile = "dipDRC_example_data.csv", method = "curl")
+    destfile = temp, method = "curl")
    
-
 # load example data into object 'd'
 # if you want to load your own data, 
-# replace 'dipDRC_example_data.csv' with file name of your data
-d <- read.csv('dipDRC_example_data.csv')
+# replace temp with file name of your data
+d <- read.csv(temp)
 
+# if using example data, remove temporary file
+unlink(temp)
+rm(temp)
 
 # identify DIP rates for each cell line and condition and produce a dose-response
 # curve for each. Model fits to data are stored into the variable 'dose.resp'
